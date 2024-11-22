f73d68434b8fbb47ccf3d508de317d767dca96e8
DrumJ <tmdgh717@naver.com>
2024-11-22 10:53:11 +0900

    익명 인증 사용자 - anonymous()

--- src/main/java/study/springsecurity6/IndexController.java
+++ src/main/java/study/springsecurity6/IndexController.java
@@ -1,5 +1,9 @@
 package study.springsecurity6;
 
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.CurrentSecurityContext;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,4 +25,22 @@ public class IndexController {
         return "login Page!";
     }
 
+    @GetMapping("/anonymous")
+    public String anonymous() {
+        return "anonymous";
+    }
+
+    @GetMapping("/authentication")
+    public String authentication(Authentication authentication) {
+        if (authentication instanceof AnonymousAuthenticationToken) {
+            return "anonymous";
+        } else {
+            return "not anonymous";
+        }
+    }
+
+    @GetMapping("/anonymousContext")
+    public String anonymousContext(@CurrentSecurityContext SecurityContext context) {
+        return context.getAuthentication().getName();
+    }
 }
--- src/main/java/study/springsecurity6/config/SecurityConfig.java
+++ src/main/java/study/springsecurity6/config/SecurityConfig.java
@@ -1,24 +1,15 @@
 package study.springsecurity6.config;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.AuthenticationFailureHandler;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-
-import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
@@ -27,7 +18,15 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/anonymous").hasRole("GUEST")
+                        .requestMatchers("/anonymousContext","/authentication").permitAll()
+                        .anyRequest().authenticated())
+                .formLogin(Customizer.withDefaults())
+                .anonymous(config -> config
+                        .principal("guest")
+                        .authorities("ROLE_GUEST")
+                )
                 .build();
     }
 
