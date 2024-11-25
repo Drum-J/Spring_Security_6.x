6d0c7cbf93343bf95cf0278dcff535f43b51bb8e
DrumJ <tmdgh717@naver.com>
2024-11-25 11:06:01 +0900

    인증 컨텍스트 - SecurityContext / SecurityContextHolder

--- src/main/java/study/springsecurity6/IndexController.java
+++ src/main/java/study/springsecurity6/IndexController.java
@@ -6,8 +6,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class IndexController {
 
+    private final SecurityContextService securityContextService;
+
+    public IndexController(SecurityContextService securityContextService) {
+        this.securityContextService = securityContextService;
+    }
+
+
     @GetMapping("/")
     public String index() {
+        securityContextService.securityContext();
+
         return "index";
     }
 
