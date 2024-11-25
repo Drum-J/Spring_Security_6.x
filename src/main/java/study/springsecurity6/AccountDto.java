72dd88bc240af26d3a3ce7cb9c15908dc80b0147
DrumJ <tmdgh717@naver.com>
2024-11-25 18:21:54 +0900

    사용자 상세 - UserDetails

---
+++ src/main/java/study/springsecurity6/AccountDto.java
@@ -0,0 +1,15 @@
+package study.springsecurity6;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Collection;
+
+@Getter
+@AllArgsConstructor
+public class AccountDto {
+    private String username;
+    private String password;
+    private Collection<GrantedAuthority> authorities;
+}
--- src/main/java/study/springsecurity6/CustomUserDetailsService.java
+++ src/main/java/study/springsecurity6/CustomUserDetailsService.java
@@ -1,17 +1,18 @@
 package study.springsecurity6;
 
-import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
+import java.util.List;
+
 public class CustomUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return User
-                .withUsername("seungho")
-                .password("{noop}test123")
-                .roles("USER")
-                .build();
+        AccountDto accountDto =
+                new AccountDto("seungho","{noop}test123", List.of(new SimpleGrantedAuthority("ROLE_USER")));
+
+        return new CustomUserDetails(accountDto);
     }
 }
