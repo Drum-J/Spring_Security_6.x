af1c6faa395f10a882a629b522b6cc44bae15cb6
DrumJ <tmdgh717@naver.com>
2024-11-25 14:41:02 +0900

    인증 관리자 - AuthenticationManager

--- src/main/java/study/springsecurity6/config/SecurityConfig2.java
+++ src/main/java/study/springsecurity6/config/SecurityConfig2.java
@@ -1,9 +1,16 @@
 package study.springsecurity6.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AnonymousAuthenticationProvider;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -11,35 +18,39 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import study.springsecurity6.CustomAuthenticationFilter;
+import study.springsecurity6.CustomAuthenticationProvider;
 
-//@Configuration
-//@EnableWebSecurity
-public class SecurityConfig {
+import java.util.List;
 
-    //@Bean
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig2 {
+
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
-        AuthenticationManagerBuilder builder = http.getSharedObject(AuthenticationManagerBuilder.class);
-        AuthenticationManager authenticationManager = builder.build();
-        //AuthenticationManager authenticationManager = builder.getObject(); // build 이후에 다른 곳에서 매니저 객체를 사용하려면 getObject() 를 사용해라!
-
         return http
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.GET,"/", "/api/login").permitAll()
                         .anyRequest().authenticated())
-                .authenticationManager(authenticationManager)
-                .addFilterBefore(customAuthenticationFilter(http, authenticationManager), UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(customAuthenticationFilter(http), UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
-    public CustomAuthenticationFilter customAuthenticationFilter(HttpSecurity http, AuthenticationManager authenticationManager) {
+    @Bean
+    public CustomAuthenticationFilter customAuthenticationFilter(HttpSecurity http) {
+        List<AuthenticationProvider> list1 = List.of(new DaoAuthenticationProvider());
+        ProviderManager parent = new ProviderManager(list1);
+
+        List<AuthenticationProvider> list2 = List.of(new AnonymousAuthenticationProvider("key"), new CustomAuthenticationProvider());
+        ProviderManager providerManager = new ProviderManager(list2, parent);
+
         CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(http);
-        customAuthenticationFilter.setAuthenticationManager(authenticationManager);
+        customAuthenticationFilter.setAuthenticationManager(providerManager);
 
         return customAuthenticationFilter;
     }
 
-    //@Bean
+    @Bean
     public UserDetailsService userDetailsService() {
         UserDetails user = User
                 .withUsername("seungho")
