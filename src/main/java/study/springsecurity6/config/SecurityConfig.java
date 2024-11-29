e0a7c2cced82cbd7ce279dbe68e21e71d8c2f73f
DrumJ <tmdgh717@naver.com>
2024-11-29 13:23:33 +0900

    악용 보호 - CORS(Cross Origin Resource Sharing)

--- src/main/java/study/springsecurity6/config/SecurityConfig.java
+++ src/main/java/study/springsecurity6/config/SecurityConfig.java
@@ -10,6 +10,9 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -18,9 +21,25 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
+                .cors(cors -> cors.configurationSource(corsConfigurarionSource()))
                 .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
                 .formLogin(Customizer.withDefaults())
                 .build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurarionSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("http://localhost:8081"); // 특정 Origin(출처)의 요청을 허용
+        config.addAllowedMethod("*"); // // 모든 HTTP Method (GET, POST, PUT, DELETE 등)를 허용
+        config.addAllowedHeader("*"); // 모든 HTTP Header 허용
+        config.setAllowCredentials(true); // 쿠키, 인증 정보를 포함한 크로스 도메인 요청을 허용
+        config.setMaxAge(3600L); // CORS pre-flight 요청(OPTIONS 요청)에 대한 캐싱 시간(초)
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return source;
     }
 
     @Bean
