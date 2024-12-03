0942e681dd26aa9ae4d756fd06db9352969413c3
DrumJ <tmdgh717@naver.com>
2024-12-03 14:58:50 +0900

    인가 프로세스 - 메서드 기반 권한 부여 @PreFilter , @PostFilter

--- src/main/java/study/springsecurity6/Account.java
+++ src/main/java/study/springsecurity6/Account.java
@@ -2,8 +2,12 @@ package study.springsecurity6;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
+@NoArgsConstructor
 @AllArgsConstructor
 public class Account {
 
--- src/main/java/study/springsecurity6/MethodController.java
+++ src/main/java/study/springsecurity6/MethodController.java
@@ -1,15 +1,25 @@
 package study.springsecurity6;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.parameters.P;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 @RestController
+@RequiredArgsConstructor
 public class MethodController {
+
+    private final DataService dataService;
 
     @GetMapping("/admin")
     @PreAuthorize("hasAuthority('ROLE_ADMIN')")
@@ -45,7 +55,29 @@ public class MethodController {
 
     @GetMapping("/isSecure")
     @PostAuthorize("hasAuthority('ROLE_ADMIN') and returnObject.isSecure")
-    public Account isSecure(@RequestParam("name") String name,@RequestParam("secure") String secure) {
+    public Account isSecure(@RequestParam("name") String name, @RequestParam("secure") String secure) {
         return new Account(name, "Y".equals(secure));
+    }
+
+    @PostMapping("/writeList")
+    public List<Account> writeList(@RequestBody List<Account> data) {
+        return dataService.writeList(data);
+    }
+
+    @PostMapping("/writeMap")
+    public Map<String, Account> writeMap(@RequestBody List<Account> data) {
+        Map<String, Account> accountMap = data.stream()
+                .collect(Collectors.toMap(Account::getOwner, account -> account));
+        return dataService.writeMap(accountMap);
+    }
+
+    @GetMapping("/readList")
+    public List<Account> readList() {
+        return dataService.readList();
+    }
+
+    @GetMapping("/readMap")
+    public Map<String, Account> readMap() {
+        return dataService.readMap();
     }
 }
