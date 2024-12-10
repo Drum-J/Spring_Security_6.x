25729bb99c102f5eae7941eeacfcc06717f35293
DrumJ <tmdgh717@naver.com>
2024-12-10 10:44:31 +0900

    feat. logout 구현

--- src/main/java/study/springsecurity6/users/controller/LoginController.java
+++ src/main/java/study/springsecurity6/users/controller/LoginController.java
@@ -1,5 +1,10 @@
 package study.springsecurity6.users.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -16,5 +21,18 @@ public class LoginController {
         // Enum 값 전달
         // model.addAttribute("roles", AccountRole.values());
         return "login/signup";
+    }
+
+    /**
+     * 간단하게는 GET 방식도 가능하나 POST 방식을 더 권장한다.
+     */
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        Authentication authentication = SecurityContextHolder.getContextHolderStrategy().getContext().getAuthentication();
+
+        if (authentication != null) {
+            new SecurityContextLogoutHandler().logout(request, response, authentication);
+        }
+        return "redirect:/login";
     }
 }
