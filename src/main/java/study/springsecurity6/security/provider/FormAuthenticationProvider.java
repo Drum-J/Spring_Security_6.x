559c6cdb110ca861a431214d3d14f054d4004c8b
DrumJ <tmdgh717@naver.com>
2024-12-10 10:28:29 +0900

    feat. 커스텀 AuthenticationProvider 구현

---
+++ src/main/java/study/springsecurity6/security/provider/FormAuthenticationProvider.java
@@ -0,0 +1,39 @@
+package study.springsecurity6.security.provider;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+import study.springsecurity6.security.service.AccountContext;
+import study.springsecurity6.security.service.FormUserDetailsService;
+
+@Component
+@RequiredArgsConstructor
+public class FormAuthenticationProvider implements AuthenticationProvider {
+
+    private final FormUserDetailsService userDetailsService;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String loginId = authentication.getName();
+        String password = authentication.getCredentials().toString();
+
+        AccountContext accountContext = (AccountContext) userDetailsService.loadUserByUsername(loginId);
+
+        if (!passwordEncoder.matches(password, accountContext.getPassword())) {
+            throw new BadCredentialsException("비밀번호가 일치하지 않습니다.");
+        }
+
+        return new UsernamePasswordAuthenticationToken(accountContext.getAccount(),null,accountContext.getAuthorities());
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return authentication.isAssignableFrom(UsernamePasswordAuthenticationToken.class);
+    }
+}
--- src/main/java/study/springsecurity6/security/service/AccountContext.java
+++ src/main/java/study/springsecurity6/security/service/AccountContext.java
@@ -1,5 +1,6 @@
 package study.springsecurity6.security.service;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -8,6 +9,7 @@ import study.springsecurity6.entity.Account;
 import java.util.Collection;
 import java.util.List;
 
+@Getter
 @RequiredArgsConstructor
 public class AccountContext implements UserDetails {
 
