6fd1200cdfd4f19a3290805d53a2c5f4049579a1
DrumJ <tmdgh717@naver.com>
2024-12-11 17:56:32 +0900

    feat. Rest CSRF 구현

--- src/main/java/study/springsecurity6/security/config/SecurityConfig.java
+++ src/main/java/study/springsecurity6/security/config/SecurityConfig.java
@@ -87,7 +87,7 @@ public class SecurityConfig {
                         .requestMatchers("/api/admin").hasRole(ADMIN.name())
                         .anyRequest().authenticated()
                 )
-                .csrf(AbstractHttpConfigurer::disable)
+                //.csrf(AbstractHttpConfigurer::disable)
                 .addFilterBefore(restAuthenticationFilter(http, authenticationManager), UsernamePasswordAuthenticationFilter.class)
                 .authenticationManager(authenticationManager)
                 .exceptionHandling(exception -> exception
